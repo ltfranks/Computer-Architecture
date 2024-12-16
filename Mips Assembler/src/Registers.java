82caa3d5a9bfae0b273a41a774882d3198d520e6
ltfranks <114035259+ltfranks@users.noreply.github.com>
2024-12-15 18:17:24 -0800

    Mips Assembler

---
+++ Mips Assembler/src/Registers.java
@@ -0,0 +1,42 @@
+public class Registers extends lab3 {
+
+
+    public Registers() {
+
+        registers.put("pc", 0);
+        registers.put("$0", 0);
+
+        registers.put("$v0", 0);
+        registers.put("$v1", 0);
+
+        registers.put("$a0", 0);
+        registers.put("$a1", 0);
+        registers.put("$a2", 0);
+        registers.put("$a3", 0);
+
+        registers.put("$t0", 0);
+        registers.put("$t1", 0);
+        registers.put("$t2", 0);
+        registers.put("$t3", 0);
+        registers.put("$t4", 0);
+        registers.put("$t5", 0);
+        registers.put("$t6", 0);
+        registers.put("$t7", 0);
+
+        registers.put("$s0", 0);
+        registers.put("$s1", 0);
+        registers.put("$s2", 0);
+        registers.put("$s3", 0);
+        registers.put("$s4", 0);
+        registers.put("$s5", 0);
+        registers.put("$s6", 0);
+        registers.put("$s7", 0);
+
+        registers.put("$t8", 0);
+        registers.put("$t9", 0);
+
+        registers.put("$sp", 0);
+        registers.put("$ra", 0);
+    }
+
+}
---
+++ Mips Assembler/src/lab3.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class lab3 {
+
+    public static HashMap<String, Integer> registers = new HashMap<>();
+    public static HashMap<String, Integer> LabelLocation = new HashMap<>();
+    public static ArrayList<List<String>> cleanedFile = new ArrayList<>();
+    public static int LineCount = 0;
+    public static int LabelTracker = 0;
+
+    public static int[] data = new int[8192];
+
+    public static void main(String[] args) throws IOException {
+        new Registers();
+
+        if (args.length == 1) {
+            new FirstScan(args[0]);
+            new ScanAssembly(args[0]);
+            new InteractiveMode();
+        } else {
+            new ScanAssembly(args[0]);
+            new InteractiveMode(args[1]);
+        }
+    }
+}
